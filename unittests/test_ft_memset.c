4f9fa55510f6a82c458b1edf50fd6b5a57b9d6b9
juan de nagykallo <juan.denagykallo@gmail.com>
2024-08-03 15:07:18 -0600

    Memset null pointer protection no SEGMENT FAULT

--- unittests/test_ft_memset.c
+++ unittests/test_ft_memset.c
@@ -50,21 +50,21 @@ void test_memset_large_size() {
 // Note: Testing memset with a null pointer would normally cause a segmentation fault.
 // This test is shown for completeness but should not be run in practice.
 // Uncommenting and running this test will cause the program to crash.
-/*
+
 void test_memset_null_pointer() {
     char *buffer = NULL;
-    memset(buffer, 'C', 10);
+    ft_memset(buffer, 'C', 10);
     // This will likely cause a segmentation fault
     printf("test_memset_null_pointer passed\n");
 }
-*/
+
 
 int test_ft_memset() {
     test_memset_basic_functionality();
     test_memset_zero_length();
     test_memset_boundary_conditions();
     test_memset_large_size();
-    // test_memset_null_pointer(); // Uncomment to test null pointer case (will crash)
+    test_memset_null_pointer(); // Uncomment to test null pointer case (will crash)
 
 printf("All tests passed!\n");
     return 0;
